29727eb753ce0420c1bcb838c865cee5750ec224
mtzjrg <mtzjrg@proton.me>
2025-12-11 21:11:51 -0600

    add basic llm interaction

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 def main():
-    print("Hello from bootdev-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("RuntimeError: no api key found")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = messages
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("RuntimeError: failed API request occurred")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
